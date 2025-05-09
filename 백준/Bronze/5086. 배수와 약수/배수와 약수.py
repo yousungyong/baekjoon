c0fac188b36367beac96561fc08164431442b6c5
yousungyong <92283264+yousungyong@users.noreply.github.com>
2025-05-09 14:36:57 +0900

    [Bronze III] Title: 배수와 약수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.py
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -1,19 +1,10 @@
-def is_divisor(a, b):
-    return b % a == 0
-
-def is_multiple(a, b):
-    return a % b == 0
-
-def process_input(a, b):
-    if is_divisor(a, b):
-        print("factor")
-    elif is_multiple(a, b):
-        print("multiple")
-    else:
-        print("neither")
-
 while True:
     a, b = map(int, input().split())
     if a == 0 and b == 0:
         break
-    process_input(a, b)
+    if b % a == 0:
+        print("factor")
+    elif a % b == 0:
+        print("multiple")
+    else:
+        print("neither")
